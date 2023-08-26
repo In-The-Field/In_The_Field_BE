78ab06a9986f6f821e54b54ceca231aa4bea2463
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-08-26 13:47:31 -0600

    add welcome route and view.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   post "/graphql", to: "graphql#execute"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "welcome#index"
 end
