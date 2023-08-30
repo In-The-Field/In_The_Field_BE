e551c8a8fee1c4dbc9e7d42c1ec4d8945f7f86f2
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-08-30 09:38:16 -0600

    add: Match Type.

---
+++ app/graphql/types/match_type.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module Types
+  class MatchType < Types::BaseObject
+    # field :id, ID, null: true
+    field :probability, Integer, null: true
+
+    field :psychoactive, Boolean, null: true
+
+    field :api_id, String, null: true
+    field :common_name, String, null: true
+    field :latin_name, String, null: true
+    field :edibility, String, null: true
+    field :photo, String, null: true
+    field :photo_citation, String, null: true
+    field :description, String, null: true
+    
+    # field :taxonomy, Hash, null: true
+    field :taxonomy, Types::TaxonomyType, null: true
+    field :characteristic, Types::CharacteristicType, null: true
+    field :look_alike, [Types::LookAlikeType], null: true
+  end
+end
