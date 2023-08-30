3ea3a9e56ba157600ea9a85058e1b5ef5b3d2d6f
Crow Rising <miscbyrd@gmail.com>
2023-08-30 16:43:10 -0600

    test: add test for graphqlschema

---
+++ spec/graphql/in_the_field_schema_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe InTheFieldBeSchema do
+  describe '.resolve_type' do
+    it 'raises an error for abstract type resolution' do
+      expect {
+        described_class.resolve_type(nil, nil, nil)
+      }.to raise_error(GraphQL::RequiredImplementationMissingError)
+    end
+  end
+
+  describe '.id_from_object' do
+    it 'returns a string UUID for an object' do
+      object = double(to_gid_param: 'fake_uuid')
+      result = described_class.id_from_object(object, nil, nil)
+      expect(result).to eq('fake_uuid')
+    end
+  end
+
+  describe '.object_from_id' do
+    it 'finds the object from a string UUID' do
+      object = double
+      allow(GlobalID).to receive(:find).with('fake_uuid').and_return(object)
+      result = described_class.object_from_id('fake_uuid', nil)
+      expect(result).to eq(object)
+    end
+  end
+end
