e3d0f092ef910fa90a3e585bc7a134e91a887420
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-08-30 09:39:45 -0600

    add: additional details and comment out similiar_images.

--- app/services/kindwise_service.rb
+++ app/services/kindwise_service.rb
@@ -12,15 +12,14 @@ class KindwiseService
       images: [base_64_image],  # Wrap the image in an array
       # latitude: 49.207,
       # longitude: 16.608,
-      similar_images: true
+      # similar_images: true
     }
 
     response = conn.post do |req|
       req.headers['Api-Key'] = ENV['KINDWISE_API']  # Make sure the API key is set in the headers
       req.body = payload.to_json
-      req.params['details'] = 'common_names'
+      req.params['details'] = 'common_names,description,edibility,image,taxonomy,psychoactive,characteristic,look_alike,description'
     end
-
     JSON.parse(response.body, symbolize_names: true)
   end
 end
