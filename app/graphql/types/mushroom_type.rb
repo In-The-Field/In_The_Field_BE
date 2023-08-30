12afb9642e13b0bc032a4cb618d9caf872e720a1
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-08-30 15:26:19 -0600

    add: initial Mushroom Type.

---
+++ app/graphql/types/mushroom_type.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module Types
+  class MushroomType < Types::BaseObject
+    field :id, ID, null: false
+    
+    field :probability, Float, null: true
+
+    field :psychoactive, Boolean, null: true
+
+    field :api_id, String, null: true
+
+    field :common_name, String, null: true
+
+    field :latin_name, String, null: true
+    field :edibility, String, null: true
+    field :photo, String, null: true
+    field :photo_citation, String, null: true
+    field :description, String, null: true
+    
+    # field :taxonomy, Types::TaxonomyType, null: true
+    # field :characteristic, Types::CharacteristicType, null: true
+    # field :look_alike, [Types::LookAlikeType], null: true
+  end
+end
