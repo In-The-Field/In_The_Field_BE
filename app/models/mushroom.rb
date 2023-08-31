2d20b0eb43f258d475d086d94bbf0302207b946c
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-08-31 12:08:59 -0600

    revised: relationship from Mushroom to children.

--- app/models/mushroom.rb
+++ app/models/mushroom.rb
@@ -1,18 +1,15 @@
 class Mushroom < ApplicationRecord
   validates :probability, presence: true
-  validates :psychoactive, presence: true
   validates :api_id, presence: true
   validates :common_name, presence: true
   validates :latin_name, presence: true
-  validates :edibility, presence: true
   validates :photo, presence: true
   validates :photo_citation, presence: true
   validates :description, presence: true
 
   has_many :user_mushrooms
   has_many :users, through: :user_mushrooms
-  has_one :characteristic, dependent: :destroy
-  has_one :taxonomy, dependent: :destroy
-  has_one :look_alike, dependent: :destroy
-
+  has_many :characteristics, dependent: :destroy
+  has_many :taxonomies, dependent: :destroy
+  has_many :look_alikes, dependent: :destroy
 end
