b93f751ebeb067376a2cb80cbd43200e61d98b18
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-08-31 12:16:35 -0600

    add: code to unfreeze array and hash data structures.

--- app/poros/match.rb
+++ app/poros/match.rb
@@ -14,7 +14,6 @@ class Match
 
 
   def initialize(data)
-    # @access_token = access_token
     @probability = data[:probability]
 
     @psychoactive = data[:details][:psychoactive]
@@ -28,26 +27,7 @@ class Match
     @description = data[:details][:description][:value]
 
     @taxonomyp = data[:details][:taxonomy]
-    @characteristicp = data[:details][:characteristic]
-    @look_alikep = data[:details][:look_alike]
-    # @look_alike = format_look_alike(data[:details][:look_alike])
-  end
-
-  # def format_probability(probability_float)
-  #   probability = (probability_float * 100).round.to_i
-  #   if probability == 100
-  #     probability = 99
-  #   end
-  #   probability
-  # end
-
-  # def format_look_alike(look_alike_data)
-  #   # names = look_alike_data.map do |look_alike|
-  #   #   look_alike[:name]  
-  #   # end
-  #   # look_alike_names.join(", ")
-  #   names = look_alike_data.map { |look_alike| look_alike[:name] }
-  #   result = names.join(', ')
-  # end
-  
+    @characteristicp = data[:details][:characteristic].to_h.deep_dup
+    @look_alikep = data[:details][:look_alike].to_a.deep_dup
+  end  
 end
