4f58fa03475fcd06621e4fbfb9da5d3cf2999882
Crow Rising <miscbyrd@gmail.com>
2023-08-31 14:06:41 -0600

    refactor:rubocop sweep

--- db/migrate/20230828192530_remove_uid_from_users.rb
+++ db/migrate/20230828192530_remove_uid_from_users.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class RemoveUidFromUsers < ActiveRecord::Migration[7.0]
   def change
     remove_column :users, :uid, :string
--- db/migrate/20230830174554_create_user_mushrooms.rb
+++ db/migrate/20230830174554_create_user_mushrooms.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreateUserMushrooms < ActiveRecord::Migration[7.0]
   def change
     create_table :user_mushrooms do |t|
