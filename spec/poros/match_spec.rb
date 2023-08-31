7e33a9d02874c1a3735ed457bc20e98a6a15f66e
Sarah Garlock <garlock.sarah10@gmail.com>
2023-08-31 11:48:55 -0600

    add spec for match poro

---
+++ spec/poros/match_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe Match do
+  describe '#initialize' do
+    it 'initializes with correct attributes' do
+      data = {
+        probability: 0.8,
+        details: {
+          psychoactive: true,
+          common_names: ['Common Name'],
+          edibility: 'Edible',
+          image: {
+            value: 'image_url',
+            citation: 'Image Citation'
+          },
+          description: {
+            value: 'Description text'
+          },
+          taxonomy: 'Plantae',
+          characteristic: 'Characteristic text',
+          look_alike: 'Look-alike text'
+        },
+        id: '12345',
+        name: 'Latin Name'
+      }
+
+      match = Match.new(data)
+
+      expect(match.probability).to eq(0.8)
+      expect(match.psychoactive).to eq(true)
+      expect(match.api_id).to eq('12345')
+      expect(match.common_name).to eq('Common Name')
+      expect(match.latin_name).to eq('Latin Name')
+      expect(match.edibility).to eq('Edible')
+      expect(match.photo).to eq('image_url')
+      expect(match.photo_citation).to eq('Image Citation')
+      expect(match.description).to eq('Description text')
+      expect(match.taxonomyp).to eq('Plantae')
+      expect(match.characteristicp).to eq('Characteristic text')
+      expect(match.look_alikep).to eq('Look-alike text')
+    end
+  end
+end
