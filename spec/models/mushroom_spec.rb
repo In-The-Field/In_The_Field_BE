cb56f815d052a8703f4522a8c1fea4f4359c23e9
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-08-31 12:15:05 -0600

    revised: testing based on new child relationships.

--- spec/models/mushroom_spec.rb
+++ spec/models/mushroom_spec.rb
@@ -3,11 +3,9 @@ require "rails_helper"
 RSpec.describe Mushroom, type: :model do
   describe "validations" do
     it { should validate_presence_of :probability }
-    it { should validate_presence_of :psychoactive }
     it { should validate_presence_of :api_id }
     it { should validate_presence_of :common_name }
     it { should validate_presence_of :latin_name }
-    it { should validate_presence_of :edibility }
     it { should validate_presence_of :photo }
     it { should validate_presence_of :photo_citation }
     it { should validate_presence_of :description }
@@ -16,8 +14,8 @@ RSpec.describe Mushroom, type: :model do
   describe "relationships" do
     it { should have_many :user_mushrooms }
     it { should have_many(:users).through(:user_mushrooms) }
-    it { should have_one :characteristic }
-    it { should have_one :taxonomy }
-    it { should have_one :look_alike }
+    it { should have_many :characteristics }
+    it { should have_many :taxonomies }
+    it { should have_many :look_alikes }
   end
 end
