2d5cdb98d287530162acc8c9c60ead003b856df9
Julian Beldotti <julian.beldotti@gmail.com>
2023-09-01 15:03:32 -0600

    Feat: Add mushrooms and saved_mushrooms fields

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -5,6 +5,8 @@ module Types
     field :id, ID, null: false
     field :name, String, null: true
     field :email, String, null: true
+    field :mushrooms, [Types::MushroomType], null: true
+    field :saved_mushrooms, [Types::MushroomType], null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
