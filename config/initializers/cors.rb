eaa4874a1288f7b084ed15e580f1960b964d694e
Crow Rising <miscbyrd@gmail.com>
2023-09-04 16:44:37 -0600

    fix: cors permissions

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    origins 'https://in-the-field-fe.vercel.app/'
 
     resource '*',
              headers: :any,
